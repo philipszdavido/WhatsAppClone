62d56fd25bb5f60a18a355dab8ba3ed2a9347f2c
Chidume Nnamdi <kurtwanger40@gmail.com>
2025-06-15 21:21:48 +0100

    Added views

--- WhatsApp Clone/Views/SettingsUIView.swift
+++ WhatsApp Clone/Views/SettingsUIView.swift
@@ -8,8 +8,173 @@
 import SwiftUI
 
 struct SettingsUIView: View {
+    
+    @State var searchText: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                
+                Section {
+                    
+                    HStack(alignment: .center) {
+                        Circle()
+                            .frame(width: 44, height: 44)
+                        VStack(alignment: .leading) {
+                            Text("John Doe")
+                            Text("johndoe")
+                        }
+                        Spacer()
+                        Image(systemName: "barcode.viewfinder")
+                    }
+                    
+                    NavigationLink(destination: EmptyView()) {
+                        HStack {
+                            Image(systemName: "face")
+                            Text("Avatar")
+                        }
+                    }
+                    
+                }
+                
+                Section {
+                    
+                    NavigationLink(destination: EmptyView()) {
+                        
+                        HStack {
+                            Image(systemName: "avatar")
+                            Text("Lists")
+                        }
+                        
+                    }
+
+                    NavigationLink(destination: EmptyView()) {
+                        
+                        HStack {
+                            Image(systemName: "avatar")
+                            Text("Broadcast messages")
+                        }
+                        
+                    }
+                    NavigationLink(destination: EmptyView()) {
+                        
+                        HStack {
+                            Image(systemName: "avatar")
+                            Text("Starred")
+                        }
+                        
+                    }
+                    NavigationLink(destination: EmptyView()) {
+                        
+                        HStack {
+                            Image(systemName: "avatar")
+                            Text("Linked devices")
+                        }
+                        
+                    }
+
+                }
+
+                Section {
+                    
+                    NavigationLink(destination: EmptyView()) {
+                        
+                        HStack {
+                            Image(systemName: "avatar")
+                            Text("Account")
+                        }
+                        
+                    }
+
+                    NavigationLink(destination: EmptyView()) {
+                        
+                        HStack {
+                            Image(systemName: "avatar")
+                            Text("Privacy")
+                        }
+                        
+                    }
+                    NavigationLink(destination: EmptyView()) {
+                        
+                        HStack {
+                            Image(systemName: "avatar")
+                            Text("Chats")
+                        }
+                        
+                    }
+                    NavigationLink(destination: EmptyView()) {
+                        
+                        HStack {
+                            Image(systemName: "avatar")
+                            Text("Notifications")
+                        }
+                        
+                    }
+                    NavigationLink(destination: EmptyView()) {
+                        
+                        HStack {
+                            Image(systemName: "avatar")
+                            Text("Storage and data")
+                        }
+                        
+                    }
+
+                }
+                
+                Section {
+                    
+                    NavigationLink(destination: EmptyView()) {
+                        
+                        HStack {
+                            Image(systemName: "avatar")
+                            Text("Help")
+                        }
+                        
+                    }
+                    NavigationLink(destination: EmptyView()) {
+                        
+                        HStack {
+                            Image(systemName: "avatar")
+                            Text("Invite a friend")
+                        }
+                        
+                    }
+                }
+                
+                Section("Also from Meta") {
+                    NavigationLink(destination: EmptyView()) {
+                        
+                        HStack {
+                            Image(systemName: "instagram")
+                            Text("Open Instagram")
+                        }
+                        
+                    }
+                    NavigationLink(destination: EmptyView()) {
+                        
+                        HStack {
+                            Image(systemName: "avatar")
+                            Text("Open Facebook")
+                        }
+                        
+                    }
+                    NavigationLink(destination: EmptyView()) {
+                        
+                        HStack {
+                            Image(systemName: "avatar")
+                            Text("Open Threads")
+                        }
+                        
+                    }
+
+                }
+                
+
+            }.navigationTitle("Settings")
+                .listRowSeparator(.hidden)
+                .listSectionSeparator(.hidden)
+                .searchable(text: $searchText)
+        }
     }
 }
 
