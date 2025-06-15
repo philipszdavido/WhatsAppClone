7d4e0ce28dab7e80e2bfcb15ba186203f4643138
Chidume Nnamdi <kurtwanger40@gmail.com>
2025-06-15 18:43:59 +0100

    Added UI views

---
+++ WhatsApp Clone/Data/Contacts.swift
@@ -0,0 +1,20 @@
+//
+//  Names.swift
+//  WhatsApp Clone
+//
+//  Created by Chidume Nnamdi on 15/06/2025.
+//
+
+import Foundation
+
+let contacts = [
+    "Chidume Nnamdi",
+    "Nnamdi Chidume",
+    "Chidi Nnamdi",
+    "Nnamdi Chidi",
+    "Chidume Nnamdi",
+    "Nnamdi Chidume",
+    "Chidi Nnamdi",
+    "Nnamdi Chidi",
+    "Kene"
+]
